8eb9cc8ae2994aa16bca2f28fec6548b7f8d6306
Nishant kashyap <sannukashyap24@gmail.com>
2022-08-21 12:04:58 +0530

    basic setup

---
+++ client/src/views/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () =>{
+
+    return(
+        <div className=''>
+            this  is home page svbasdvasda
+        </div>
+    )
+}
+
+export default Home;
---
+++ client/src/views/Register.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Register  = () =>{
+
+    return(
+        <div>
+            this is Register page
+        </div>
+    )
+}
+
+export default Register;
